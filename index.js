a897fc1a8fae5a96cbbb424293a8776a3a139c23
dmsch <dschukin@mail.ru>
2017-11-05 19:53:19 +0200

    update

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(express.static(__dirname + '/dist.dev'));
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
 
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(request, response) {
   response.render(__dirname + '/dist.dev/index.html');
 });
