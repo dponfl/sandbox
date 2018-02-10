aa7b4c76f13c32cd7e17363866aee9ccca5cd9c8
dmsch <dschukin@mail.ru>
2018-02-10 20:25:15 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+console.log('!!!!!!!!!!!!!!!!!!!!!!');
+console.log('PORT: ' + process.env.PORT);
+console.log('__dirname: ' + __dirname);
+console.log('!!!!!!!!!!!!!!!!!!!!!!');
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.set('view options', {layout: false});
